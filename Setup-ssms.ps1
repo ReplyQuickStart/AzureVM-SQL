9fdf6568e0a8cbe0a04e8bbede811305dfd5edec
Rafael Arnosti <r.brucknerarnosti@reply.eu>
2019-12-13 19:56:31 +0000

    v5

--- Setup-ssms.ps1
+++ Setup-ssms.ps1
@@ -1,12 +1,24 @@
-<# Custom Script for Windows to install a file from Azure Storage using the staging folder created by the deployment script #>
-param (
-    [string]$artifactsLocation,
-    [string]$artifactsLocationSasToken,
-    [string]$folderName,
-    [string]$fileToInstall
-)
-
-$source = $artifactsLocation + "\$folderName\$fileToInstall" + $artifactsLocationSasToken
-$dest = "C:\WindowsAzure\$folderName"
-New-Item -Path $dest -ItemType directory
-Invoke-WebRequest $source -OutFile "$dest\$fileToInstall"
+# Set file and folder path for SSMS installer .exe
+$folderpath="c:\windows\temp"
+$filepath="$folderpath\SSMS-Setup-ENU.exe"
+ 
+#If SSMS not present, download
+if (!(Test-Path $filepath)){
+write-host "Downloading latest SSMS..."
+$URL = "https://download.microsoft.com/download/3/1/D/31D734E0-BFE8-4C33-A9DE-2392808ADEE6/SSMS-Setup-ENU.exe"
+$clnt = New-Object System.Net.WebClient
+$clnt.DownloadFile($url,$filepath)
+Write-Host "SSMS installer download completed" -ForegroundColor Green
+ 
+}
+else {
+ 
+write-host "Found setup file, moving on to install..."
+}
+ 
+# start the SSMS installer
+write-host "Installing SSMS...." -nonewline
+$Parms = " /Install /Quiet /Norestart /Logs log.txt"
+$Prms = $Parms.Split(" ")
+& "$filepath" $Prms | Out-Null
+Write-Host "SSMS installation complete" -ForegroundColor Green
